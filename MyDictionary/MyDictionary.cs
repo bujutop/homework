3497f6a61aba0884be37ce908f243d0eb7a51a15
YBT <YBT@DESKTOP-6QHQD6V>
2021-01-23 02:43:22 +0300

    dictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey,Tval>
+    {
+        Tkey[] _keys;
+        Tval[] _values;
+
+        public MyDictionary() //initialize
+        {
+             _keys = new Tkey[0];
+             _values = new Tval[0];
+
+        }
+
+        public void Add(Tkey key, Tval value)
+        {
+            Tkey[] _tempKeys = _keys;
+            Tval[] _tempValues = _values;
+
+            _keys = new Tkey[_keys.Length + 1];
+            _values = new Tval[_values.Length + 1];
+
+            for (int i = 0; i < _tempKeys.Length; i++)
+            {
+                _keys[i] = _tempKeys[i];
+                _values[i] = _tempValues[i];
+
+            }
+
+            _keys[_tempKeys.Length] = key;
+            _values[_tempValues.Length] = value;
+        }
+
+            
+
+        public int Count
+        {
+            get { return _keys.Length; }
+            
+        }
+
+        
+
+        public Tkey[] Keys
+        {
+            get { return _keys; }
+            
+        }
+
+
+
+        public Tval[] Values
+        {
+            get { return _values; }
+            
+        }
+
+
+
+
+
+    }
+
+    
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> musteriListesi = new MyDictionary<int, string>();
+            musteriListesi.Add(00123, "Nurettin Ayıpettin");
+            musteriListesi.Add(456836, "Bazı İnsanlar");
+            musteriListesi.Add(452645, "Çeşitli İsimler");
+
+            Console.WriteLine("Toplam müşteri sayısı: {0}",musteriListesi.Count);
+
+            for(int i = 0; i < musteriListesi.Count; i++) 
+            {
+                Console.WriteLine("Müşteri No: {0}",musteriListesi.Keys[i]);
+               
+            }
+
+            foreach (var values in musteriListesi.Values)
+            {
+                Console.WriteLine("Müsteri Adı: {0}",values);
+            }
+            
+
+            
+
+
+           
+        }
+    }
+}
